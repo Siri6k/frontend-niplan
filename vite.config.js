37b10c9c0dd2414a70a13c349130a9d39a9b2f38
sirisk <adamchrisk@gmail.com>
2026-02-12 13:06:37 +0200

    update css for safari

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite"; // <-- import du plugin Tailwind
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   plugins: [
+    legacy({
+      targets: ["defaults", "not IE 11", "Safari >= 12"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+      modernPolyfills: true,
+    }),
     react(),
     tailwindcss(), // ← ajoutez ce plugin
   ],
@@ -13,6 +19,7 @@ export default defineConfig({
     open: true,
   },
   build: {
+    target: "es2015",
     outDir: "build", // même nom que CRA
     emptyOutDir: true,
   },
